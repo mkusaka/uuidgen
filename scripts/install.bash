590745711b8213af7a944ca5fe3be8f0c68f254c
mkusaka <hinoshita1992@gmail.com>
2022-05-03 14:06:48 +0900

    v0.0.4

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-version="${1:-0.0.3}"
+version="${1:-$(curl https://api.github.com/repos/mkusaka/uuidgen/releases/latest -s | jq .name -r | sed -e s/^v//g)}"
 
 if [[ ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "Given version '${version}' does not match to regex" '^[0-9]+\.[0-9]+\.[0-9]+$' >&2
